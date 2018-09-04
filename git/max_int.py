70db2f50098b87edd04bf360853e92b83b5b7b67
Stefan Mani <vma424630@vma.is>
2018-09-04 14:19:59 +0000

    Eingongu Algrímur

--- git/max_int.py
+++ git/max_int.py
@@ -1,2 +1,6 @@
-"Peter is happy"
-"But susan is not happy"
+# Gera lykkju til að leifa fjöld inputta
+# Tekur input frá user og setur í lista
+# Spyr aftur og aftur þanga til að sett er inn neikvæð tala
+# Gera svo lykkju sem finnur stæðstu tölunna
+
+
