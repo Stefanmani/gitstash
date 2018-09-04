8f6ae52cb2d84d4c2c414f898f389cd7d8bd18c8
Stefan Mani <vma424630@vma.is>
2018-09-04 14:35:28 +0000

    Finished and tested

--- git/max_int.py
+++ git/max_int.py
@@ -7,8 +7,7 @@ while True:
     num_int = int(input("Input a number: "))    # Do not change this line
     if num_int < 0:
         break
-    if num_int > big_num:
-        max_int == num_int
-    
+    if num_int > max_int:
+        max_int = num_int
 # Fill in the missing code
 print("The maximum is", max_int)    # Do not change this line
